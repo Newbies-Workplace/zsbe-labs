c546b2e8a8d0304a712ff177df6a19de02e704ad
JUtupe <petryszyn@o2.pl>
2023-01-24 14:21:51 +0100

    lab 02

---
+++ labs/02-tic-tac-toe/src/exercise.js
@@ -0,0 +1,7 @@
+const input = require('prompt-sync')();
+const print = (a) => process.stdout.write(a)
+
+const VALUE_EMPTY = ' '
+const VALUE_COMPUTER = 'X'
+const VALUE_PLAYER = 'O'
+const SIZE = 3
